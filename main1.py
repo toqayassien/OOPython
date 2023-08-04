0f8f603cef30f1db3ba35ab8126fdef709921922
toqayassien <122563592+toqayassien@users.noreply.github.com>
2023-08-04 23:40:31 +0300

    Added more comments and some main changes

--- main1.py
+++ main1.py
@@ -6,10 +6,9 @@ def main():
     student1.accept()
     student2 = Students("Ron", 456, 96, 50)
     student2.accept()
-    student1.update(123, 789)
-    student1.display()
-    Students.search(456)
-
+    Students.delete(123)
 
 if __name__ == "__main__":
     main()
+
+    
